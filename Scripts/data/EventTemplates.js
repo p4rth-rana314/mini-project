710a680bc1f6066ebb6f22063e63abc3ee6cbe62
DISHOO3 <dishank.parmar@somaiya.edu>
2024-10-12 11:44:05 +0530

    latest

--- Scripts/data/EventTemplates.js
+++ Scripts/data/EventTemplates.js
@@ -6,31 +6,37 @@ if (!Events) {
       name: "Chess",
       date: "01-01-2024",
       eventId: 1,
+      players: ["dishank.parmar", "zaisha.parmar"],
     },
     {
       name: "Carrom",
       date: "02-01-2024",
       eventId: 2,
+      players: ["dishank.parmar", "zaisha.parmar"],
     },
     {
       name: "Table Tennis",
       date: "03-01-2024",
       eventId: 3,
+      players: ["dishank.parmar", "zaisha.parmar"],
     },
     {
       name: "Football",
       date: "04-01-2024",
       eventId: 4,
+      players: ["dishank.parmar", "zaisha.parmar"],
     },
     {
       name: "Table Tennis",
       date: "03-01-2024",
       eventId: 3,
+      players: ["dishank.parmar", "zaisha.parmar"],
     },
     {
       name: "Table Tennis",
       date: "03-01-2024",
-      eventId: 3,
+      eventId: 4,
+      players: ["dishank.parmar", "zaisha.parmar"],
     },
   ];
 }
